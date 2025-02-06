3159fd6af247c522b11da1448854eae504ce6f27
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-06 20:04:37 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,9 +1,15 @@
 import app from "./src/app"
+import { config } from "./src/config/config"
 
 const startServer = () => {
-     const port = process.env.PORT || 3000
+     const port = config.port // Access port from the loaded config
+     if (!port) {
+          console.error("Port is undefined. Check your .env file!")
+          process.exit(1)
+     }
      app.listen(port, () => {
           console.log(`Server Started on port ${port}`)
      })
 }
+
 startServer()
