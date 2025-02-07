21774d4430d9ee2b05f4a1d1a3383999551ac9b8
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-07 09:24:15 +0530

    mongo connection setup

---
+++ src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { config } from "./config"
+
+const connectDB = async () => {
+     try {
+          await mongoose.connect(config.databaseURL as string)
+          mongoose.connection.on("connected", () => {
+               console.log("Connected to Database Successfully")
+          })
+          mongoose.connection.on("error", (err) => {
+               console.log(`Error in connecting Database : ${err}`)
+          })
+     } catch (err) {
+          console.error("Failed to Connect to Database", err)
+          process.exit(1)
+     }
+}
+export default connectDB
